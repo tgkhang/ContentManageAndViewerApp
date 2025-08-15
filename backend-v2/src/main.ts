008fd52a3e09ec9e106a832b99ef498f3f959ec6
Khang <tgkhang22@clc.fitus.edu.vn>
2025-08-16 01:43:41 +0700

    [m] Swagger setup

--- backend-v2/src/main.ts
+++ backend-v2/src/main.ts
@@ -14,10 +14,9 @@ async function bootstrap() {
   });
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Content Management API')
+    .setDescription('The content management API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
